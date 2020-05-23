d63b1122312a2f9064b5501b9a339fe1f0274292
reed123456 <3034903604@qq.com>
2020-05-23 20:05:37 +0800

    add namespace

---
+++ ConsoleApplication1/ConsoleApplication1/test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int key;
+	cout << "ÇëÊäÈëkeyµÄÖµ £º>";
+	cin >> key;
+
+	cout << "key = " << key << endl;
+
+	system("pause");
+	return 0;
+}
+
+/*
+int main()
+{
+	int a = 10;
+	double b = 12.34;
+
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+
+	system("pause");
+	return 0;
+}
+*/
