056208b608019dede306c392c95ba7c28d6af982
reed123456 <3034903604@qq.com>
2020-06-25 20:55:33 +0800

    add

---
+++ Test_06_25/Test_06_25/test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+
+class Solution {
+public:
+	string tree2str(TreeNode* t) {
+		string str;
+
+	}
+};*/
+int main()
+{
+
+	system("pause");
+	return 0;
+}
