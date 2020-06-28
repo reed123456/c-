3e359e623328bfed21bf7b1744628b1016c299ef
reed123456 <3034903604@qq.com>
2020-06-28 21:25:31 +0800

    add

---
+++ Test_06_28/Test_06_28/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	pair<int, string> isv = { 1, "Alice" };
+	cout << isv.first << ":" << isv.second << endl;
+	system("pause");
+	return 0;
+}
